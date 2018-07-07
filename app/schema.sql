84b87ec6622e7ab3a6f19e5f6ffb8d2cba522cc3
Julius Ijie <juliusijie@gmail.com>
2018-07-07 12:30:14 +0100

    Db schema definition

---
+++ app/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS admins;
+DROP TABLE IF EXISTS agents;
+DROP TABLE IF EXISTS admin_agents;
+
+CREATE TABLE admins (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	phone TEXT NOT NULL,
+	password TEXT NOT NULL,
+	biz_name TEXT NOT NULL,
+	pass TEXT,
+	created_at DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE agents (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	phone TEXT NOT NULL,
+	password TEXT NOT NULL,
+	confirmed BOOLEAN DEFAULT 0,
+	created_at DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE admin_agents (
+	admin_id INTEGER NOT NULL,
+	agent_id INTEGER NOT NULL,
+	invited_at DEFAULT CURRENT_TIMESTAMP,
+	accepted BOOLEAN DEFAULT 0,
+	FOREIGN KEY (admin_id) REFERENCES admins (id),
+	FOREIGN KEY (agent_id) REFERENCES agents (id)
+)
+
